ba2a942ac7ed53c81866e3f7d2329f965016cfdc
Douglas Lessa <douglaslessa@wizsolucoes.com.br>
2020-09-22 16:12:11 -0300

    feat(identityserver4): add dependencies and start project

---
+++ Config.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+namespace sso_api
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope(name: "read",   displayName: "Read your data."),
+                new ApiScope(name: "write",  displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client
+                {
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientId = "xpto_app",
+                    ClientSecrets = { new Secret("xpto_segredo".Sha256()) },
+                    AllowedScopes = { "openid", "profile", "read", "write", "delete" },
+                    ClientClaimsPrefix = ""
+                }
+            };
+    }
+}
